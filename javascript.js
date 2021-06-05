f296f907d5c10bab85529aadd3e04fb865630b38
valeriudragan <draganvaleriu8@gmail.com>
2021-06-05 23:36:08 +0300

    new1

--- javascript.js
+++ javascript.js
@@ -6,6 +6,9 @@ var g = 0;
 var b = 0;	
 var x = 100;
 var height = 1000;
+var rr = Math.floor(Math.random())
+var bb = Math.floor(Math.random());
+var gg = Math.floor(Math.random());
 function ff()
 {
 	
@@ -25,14 +28,9 @@ function ff()
 		});
 	///color
 		$('body').css({
-		'background-color':'rgb(' + r + ',' + g + ',' + b + ')'
+		'background-color':'rgb(' + rr + ',' + gg + ',' + bb + ')'
 		});
-		if(r < 255)
-			r++;
-		else if(g < 255)
-			g++;
-		else
-			b++;
+	 
 	}
 
 	else
@@ -42,20 +40,39 @@ function ff()
 			'transform':'rotate(' + x + 'deg)'
 		});
 		$('body').css({
-			'background-color':'rgb(' + r + ',' + g + ',' + b + ')'
+			'background-color':'rgb(' + rr + ',' + gg + ',' + bb + ')'
 		});
-		 if(r == 255)
-		 	{if(g == 255)
-			 	b--;
-				else
-				g--;
-			}
-			else
-				r--;
+		 
 	}
 	oldscroll = newscroll;
-
-	
+	if(rr > 10 && rr < 245)
+	{
+		rr = Math.floor(Math.random()*(rr+ 10) + (rr - 10));
+		console.log( "unu",rr);
+	}
+	else if(rr <= 10)
+	{
+		rr = Math.floor(Math.random()*(rr+ 10) + rr);
+		console.log("doi",rr);
+	}
+	else if(rr >= 245)
+	{
+		rr = Math.floor(Math.random()*(245) + 1);
+		console.log("trei",rr);
+	}
+	if(gg > 10 && gg < 245)
+		gg = Math.floor(Math.random()*(gg+ 10) + gg- 10);
+	else if(rr <= 10)
+		gg = Math.floor(Math.random()*(gg+ 10) + gg);
+	else if(gg >= 245)
+		gg = Math.floor(Math.random()*(245) + 1);
+	if(bb > 10 && bb < 245)
+		bb = Math.floor(Math.random()*(245) + 1);
+	else if(bb <= 10)
+		bb = Math.floor(Math.random()*(bb+ 10) + bb);
+	else if(bb >= 245)
+		bb = Math.floor(Math.random()*(230) + 1);
+		
 }
 
 document.getElementById('b').onscroll = function() { ff()} ;
