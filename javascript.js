5af843cbbbfdc0a46bd590a7f94848b1b9c94fe6
valeriudragan <draganvaleriu8@gmail.com>
2021-06-05 22:28:00 +0300

    new1

--- javascript.js
+++ javascript.js
@@ -8,32 +8,54 @@ var x = 100;
 var height = 1000;
 function ff()
 {
+	
 	newscroll = window.scrollY;
 	if(oldscroll < newscroll)
 		height = height + newscroll - oldscroll;
-	else
-	{
 	 
-	}
 	 $('body').css({ 
 		'height': height
 	});
 	if(newscroll > oldscroll)
 	{
-	x = x + 10;
-	$("#im").css({
-		'transform':'rotate(' + x + 'deg)'
-	});
+		
+		x = x + 10;
+		$("#im").css({
+			'transform':'rotate(' + x + 'deg)'
+		});
+	///color
+		$('body').css({
+		'background-color':'rgb(' + r + ',' + g + ',' + b + ')'
+		});
+		if(r < 255)
+			r++;
+		else if(g < 255)
+			g++;
+		else
+			b++;
 	}
+
 	else
 	{
 		x = x - 10;
 		$("#im").css({
 			'transform':'rotate(' + x + 'deg)'
 		});
+		$('body').css({
+			'background-color':'rgb(' + r + ',' + g + ',' + b + ')'
+		});
+		 if(r == 255)
+		 	{if(g == 255)
+			 	b--;
+				else
+				g--;
+			}
+			else
+				r--;
 	}
 	oldscroll = newscroll;
 
+	
 }
 
 document.getElementById('b').onscroll = function() { ff()} ;
